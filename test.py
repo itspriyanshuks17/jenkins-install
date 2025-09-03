312cb0b4a0b26c1f737dc95958c938e3988507f4
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-09-03 05:37:55 +0530

    for checking email notification for failed build- now it should send that build is successful

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 # print("Just for Jenkins trial!!")
 # print("Automatic pull from Jenkins")
 
-print("for checking email notification for failed build"
+# print("for checking email notification for failed build"
+print("for checking email notification for failed build- now it should send that build is successful")
