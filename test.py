46974e33b5669471ff0f31af8f3a715d127bc2b1
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-09-05 13:25:45 +0530

    Trial for Neha

--- test.py
+++ test.py
@@ -2,4 +2,6 @@
 # print("Automatic pull from Jenkins")
 
 # print("for checking email notification for failed build"
-print("for checking email notification for failed build- now it should send that build is successful")
+# print("for checking email notification for failed build- now it should send that build is successful")
+
+print("Trial for Neha")
