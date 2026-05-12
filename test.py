4b2b63e4394be45ea1a28ec19002c7bc95b98e24
Priyanshu Kumar Sharma <priyanshu17ks@gmail.com>
2026-05-12 19:21:56 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,12 @@
 
 # print("for checking email notification for failed build"
 # print("for checking email notification for failed build- now it should send that build is successful")
-#Third trial
+#Third Trial
 
 print("Trial 4")
 print("DevOps Lecture")
 
+
+#Fourth Trial
+print("Fedora Jenkins deployed!")
+print("Jenkins and GitHub Connected")
